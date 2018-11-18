df307812269edbaf6dbc6d52d20de9ae02b2973e
yastaheran <y.taheranpour@gmail.com>
2018-11-18 12:54:18 +0100

    Create setup.py

---
+++ Python/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='temperature_CO2_plottery',
+    version= '1',
+    description='Python script which reads and generates temperature & co2 from csv files',
+    author= 'yast',
+    author_email='yast@uio.no',
+    py_modules=['temperature_CO2_plottery'],
+  )
