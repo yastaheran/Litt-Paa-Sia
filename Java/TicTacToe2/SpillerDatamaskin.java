794300eb0f0e641a74bece35366c0f16c6e92ccd
yastaheran <y.taheranpour@gmail.com>
2018-11-19 13:50:42 +0100

    Create SpillerDatamaskin.java

---
+++ Java/TicTacToe2/SpillerDatamaskin.java
@@ -0,0 +1,17 @@
+public class SpillerDatamaskin extends Spiller{
+    
+    public SpillerDatamaskin(int spiller){
+        super(spiller);
+        System.out.println("Spiller 'SpillerDatamaskin' laget");
+    }
+
+    @Override
+    public void Spill(Board board){
+
+    }
+
+    @Override
+    public void Try(Board board){
+
+    }
+}
