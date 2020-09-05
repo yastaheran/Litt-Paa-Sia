b86f5711179f8b707bfcbebf46d1f43c1a08b68b
yastaheran <y.taheranpour@gmail.com>
2020-09-05 16:27:33 +0200

    Create quizmaster2.py
    oppgavetekst

---
+++ Python/Oblig-IN1000/oblig3/quizmaster2.py
@@ -0,0 +1,6 @@
+#Oppgave 5: Egen Oppgave
+
+# Lag en quiz med multiple choice-oppgaver og kortesvarsoppgaver
+# Riktig svar = 1 poeng
+# Feil svar (kortsvarsoppgaver) = 0 poeng
+# Feil svar (multiple choice) = -2 poeng
