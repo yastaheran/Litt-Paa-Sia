b7bf8d17aff6f047964c9223765b540af89fa734
yastaheran <y.taheranpour@gmail.com>
2020-09-05 12:12:16 +0200

    Create fahrenheit_celcius.py

---
+++ Python/Oblig-IN1000/oblig2/fahrenheit_celcius.py
@@ -0,0 +1,7 @@
+#Oppgave 2: Konvertering
+
+fahrenheit = int(input("Skriv inn en tempratur i Fahrenheit: "))
+
+celsius = (fahrenheit - 32) * 5.0/9.0
+
+print ("Temperatur:", fahrenheit, "Fahrenheit = ", celsius, " C")
