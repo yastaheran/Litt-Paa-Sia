ede145ec4b61debf735c69b55f31b8d3213a08bf
yastaheran <y.taheranpour@gmail.com>
2020-09-05 11:50:24 +0200

    Create beslutninger.py

---
+++ Python/Oblig-IN1000/oblig1/beslutninger.py
@@ -0,0 +1,12 @@
+#Oppgave 2: Beslutninger
+
+print("Vil du ha en brus?")
+
+svar = input("> ");
+
+if svar == "ja":
+    print("Her har du en brus!")
+elif svar == "nei":
+    print("Den er grei.")
+else:
+    print("Det forsto jeg ikke helt.")
