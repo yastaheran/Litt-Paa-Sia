5916ad49eea03d0196e3fed58da6e95c1e275faf
yastaheran <y.taheranpour@gmail.com>
2020-09-07 21:03:22 +0200

    Create Bil1.java

---
+++ Java/IN1010-obliger/Oblig0/Bil1.java
@@ -0,0 +1,12 @@
+/**
+  Skriv klassen Bil1 med en metode som skriver ut “Jeg er en bil”. 
+*/
+
+import java.util.*;
+
+public class Bil1  {
+
+  public void printUt() {
+    System.out.println("Jeg er en bil");
+  }
+}
