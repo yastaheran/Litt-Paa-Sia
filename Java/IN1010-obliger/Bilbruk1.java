571d76d01eec18fb2df1abd59215711ac060064b
yastaheran <y.taheranpour@gmail.com>
2020-09-07 21:02:56 +0200

    Update and rename Java/IN1010-obliger/Bilbruk1.java to Java/IN1010-obliger/Oblig0/Bilbruk1.java

--- Java/IN1010-obliger/Bilbruk1.java
+++
@@ -1,3 +0,0 @@
-/* Skriv klassen Bil1 med en metode som skriver ut “Jeg er en bil”. Skriv en klasse som du
-kaller BilBruk1 med en main-metode som oppretter et bilobjekt. La til slutt main-metoden
-kalle skriv-metoden i dette bilobjektet. */
---
+++ Java/IN1010-obliger/Oblig0/Bilbruk1.java
@@ -0,0 +1,12 @@
+/**
+  Skriv en klasse som du kaller BilBruk1 med en main-metode som oppretter et
+  bilobjekt. La til slutt main-metoden kalle skriv-metoden i dette bilobjektet.
+*/
+
+public class Bilbruk1 {
+  public static void main(String args[]) {
+    Bil1 bilen = new Bil1();
+
+    bilen.printUt();
+  }
+}
