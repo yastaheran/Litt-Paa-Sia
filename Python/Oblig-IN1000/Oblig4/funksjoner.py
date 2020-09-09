2d16e2d1f2808bbe1ea32149e4e7a65ec95e735e
yastaheran <y.taheranpour@gmail.com>
2020-09-09 13:14:25 +0200

    Update funksjoner.py

--- Python/Oblig-IN1000/Oblig4/funksjoner.py
+++ Python/Oblig-IN1000/Oblig4/funksjoner.py
@@ -1,23 +1,20 @@
 #Oppgave 1: Parametere og returverdier
 
-# Del 1.1
 def adder(tall1, tall2):
     return (tall1 + tall2)
 
 print(adder(21, 29))
 print(adder(214, 123))
 
-# Del 1.2
-tekststreng = input("Skriv inn en tekststreng: ")
-bokstav = input("Skriv inn en bokstav: ")
-
-# Del 1.3
 def tellForekomst(minTekst, minBokstave):
     forekomst = 0
 
-    for x in tekststreng:
-        if tekststreng[x] == bokstav:
-            forekomst++
-    print(bokstav + " forekommer " + " ganger i tekststreng " + tekststreng)
+    for x in minTekst:
+        if x == minBokstave:
+            forekomst += 1
 
-tellForekomst(tekstreng, bokstav)
+    print(minBokstave + " forekommer " + str(forekomst) + " ganger i tekststreng " + minTekst)
+
+tekststreng = input("Skriv inn en tekststreng: ")
+bokstav = input("Skriv inn en bokstav: ")
+tellForekomst(tekststreng, bokstav)
